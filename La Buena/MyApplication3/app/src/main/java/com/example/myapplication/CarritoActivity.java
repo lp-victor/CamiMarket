7cff9ef989e566f9137163bb93f40477efe0e2c9
Bruno Silva <brunosilvafeliz@gmail.com>
2023-11-19 13:59:30 -0800

    version carro

--- La Buena/MyApplication3/app/src/main/java/com/example/myapplication/CarritoActivity.java
+++ La Buena/MyApplication3/app/src/main/java/com/example/myapplication/CarritoActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -11,7 +12,10 @@ public class CarritoActivity extends AppCompatActivity {
 
 
         private ListView listView;
-        private ArrayList<Producto> objetos = new ArrayList<>();
+        private TextView total;
+        private double totalprec = 0;
+        private TextView totalprecio;
+        private ArrayList<Producto> carro = new ArrayList<>();
 
         @Override
         protected void onCreate(Bundle savedInstanceState) {
@@ -21,11 +25,23 @@ public class CarritoActivity extends AppCompatActivity {
             listView = findViewById(R.id.listView);
 
             // Agrega objetos a la lista
-            objetos.add(new Producto("Persona 1", "25"));
-            objetos.add(new Producto("Persona 2", "30"));
+            //carrito.add(new Producto("Persona 1", "25"));
+            //carrito.add(new Producto("Persona 2", "30"));
+            for (Producto producto: carrito) {
+                carro.add(producto);
+            }
 
-            com.example.myapplication.MiAdaptador adaptador = new com.example.myapplication.MiAdaptador(this, objetos);
+            com.example.myapplication.MiAdaptador adaptador = new com.example.myapplication.MiAdaptador(this, carro);
             listView.setAdapter(adaptador);
+
+            totalprecio = findViewById(R.id.preciototal);
+            total = findViewById(R.id.productostotal);
+
+            total.setText(carro.size());
+            for (Producto producto: carro) {
+                totalprec = totalprec + producto.getPrecio();
+            }
+
         }
 
 }
